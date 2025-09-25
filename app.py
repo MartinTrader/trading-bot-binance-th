fc6ac3626ee25cb376d3e2e58cd322bd6437e178
MartinTrader <martin.borsetti@gmail.com>
2025-09-25 08:12:50 +0700

    Update zu Flask-Webserver

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hallo, mein Trading-Roboter funktioniert!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hallo, mein Trading-Roboter funktioniert! (Test erfolgreich)"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
